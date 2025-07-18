9d0611a0746d9219d8c3193af419ae1541f75c5e
Sérgio-Damm <sergiodamm1@gmail.com>
2025-07-18 16:21:47 -0300

    Substitui axios por undici para corrigir vulnerabilidade

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const { request } = require('undici');
 const cheerio = require('cheerio');
 const cors = require('cors');
 const app = express();
@@ -10,12 +10,13 @@ app.use(cors({
 
 app.get('/transito', async (req, res) => {
     try {
-        const response = await axios.get('https://www.cetsp.com.br/', {
+        const { body } = await request('https://www.cetsp.com.br/', {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
         });
-        const $ = cheerio.load(response.data);
+        const responseData = await body.text();
+        const $ = cheerio.load(responseData);
 
         const norte = $('.info.norte h4').text().replace(' km', '') || '0';
         const oeste = $('.info.oeste h4').text().replace(' km', '') || '0';
